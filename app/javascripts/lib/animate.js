29ed089b3373724982c0ac23a4badb03a8f40a51
dzucconi <mail@damonzucconi.com>
2016-10-06 22:47:12 +0200

    Intercept on each & cycle

--- app/javascripts/lib/animate.js
+++ app/javascripts/lib/animate.js
@@ -4,8 +4,13 @@ export default frames => {
   let n = -1;
 
   const r = {
-    run: fn =>
-      fn(r.step(), () => r.run(fn)),
+    run: ({ step, cycle }) => {
+      if (n === -1 || n === frames.length - 1) cycle();
+
+      step(r.step(), () => {
+        r.run({ step, cycle });
+      });
+    },
 
     step: () =>
       frames[n = over(n, frames.length)],
